a45de88187e3ce71f56d547ef40a1437fcd3a699
keishi-yamamoto <keishi11433@gmail.com>
2020-05-19 05:47:51 +0000

    [Add]devise_controllers/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: "users/devise/sessions",
+    passwords: "users/devise/passwords",
+    registrations: "users/devise/registrations"
+  }
+  devise_for :admins, controllers: {
+    sessions: "admins/devise/sessions",
+    passwords: "adminss/devise/passwords",
+    registrations: "admins/devise/registrations"
+  }
 end
