3b5c8afe1710c46f54072a7dac0e10fba6f6f6ad
keishi-yamamoto <keishi11433@gmail.com>
2020-06-06 16:53:34 +0000

    [Modify]本棚選択時の条件文修正

--- app/controllers/users/user_titles_controller.rb
+++ app/controllers/users/user_titles_controller.rb
@@ -32,19 +32,33 @@ class Users::UserTitlesController < ApplicationController
   end
 
   def create
-    # 既存の本棚を選んだかどうか
-    if params[:option] && params[:option] = "choice"
-      byebug
+    # 本棚に関する操作
+    # ユーザが既存の本棚を選択した場合
+    if params[:option] == "choice"
+      @book_shelf = BookShelf.find(params[:BookShelf][:chosen_id])
+    # 本棚が存在しない場合、存在するが新しい本棚を作るを選択した場合
     else
       current_user.book_shelves.create!(name: params[:name])
       @book_shelf = current_user.book_shelves.last
     end
+
+    # 登録する書籍に関する操作
     # DB内に既存の書籍の場合
     if params[:user_title][:id]
-      id = params[:user_title][:id]
-      current_user.user_titles.create!(title_id: id)
+      @title = Title.find(params[:user_title][:id])
+    # DB内に存在せず、新規登録する書籍の場合
+    else
+      Title.create!(
+        name: params[:user_title][:name],
+        author: params[:user_title][:author],
+        publisher: params[:user_title][:publisher]
+      )
+      @title = Title.last
     end
-    byebug
+    current_user.user_titles.create!(
+      book_shelf_id: @book_shelf.id,
+      title_id: @title.id
+    )
     redirect_to book_shelves_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_101723) do
 
   create_table "user_titles", force: :cascade do |t|
     t.integer "user_id"
+    t.integer "book_shelf_id"
     t.integer "title_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
