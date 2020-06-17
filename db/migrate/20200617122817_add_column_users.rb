2110a6114487d8a732894445d7aa1a9059b41a68
keishi-yamamoto <keishi11433@gmail.com>
2020-06-17 13:32:48 +0000

    [Modify]migration変更

--- db/migrate/20200617122817_add_column_users.rb
+++ db/migrate/20200617122817_add_column_users.rb
@@ -1,6 +1,5 @@
 class AddColumnUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :notification_type
     add_column :users, :notification_type, :integer, default: 0, null: false
   end
 end
